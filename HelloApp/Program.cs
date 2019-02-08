9d9d2748eedeb6c9092aa42ccff56efe43f3275b
user <starostenkolg@gmail.com>
2019-02-08 03:30:00 +0200

    Second update

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -6,7 +6,7 @@ namespace HelloApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Brave New World!");
         }
     }
 }
